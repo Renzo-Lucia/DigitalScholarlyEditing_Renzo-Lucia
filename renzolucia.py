d848d1458f1f16f91326c447a036a456d9e68146
alicepiazzi <alicep2401@gmail.com>
2025-09-13 10:29:02 +0200

    check xml-xslt

--- renzolucia.py
+++ renzolucia.py
@@ -1,8 +1,8 @@
 from lxml import etree
 
 # Percorsi ai file
-xml_file = '/Users/alicep/Downloads/DSE -theories, methods and practice/iPortfolio/assets/xml/ipromessisposi_cap1.xml'          # il tuo XML principale
-xsl_file = '/Users/alicep/Downloads/DSE -theories, methods and practice/iPortfolio/assets/xml/renzoluciaimproved.xsl'
+xml_file = '/Users/alicep/Documents/GitHub/DigitalScholarlyEditing_Renzo-Lucia/assets/xml/simpleIPS.xml'          # il tuo XML principale
+xsl_file = '/Users/alicep/Documents/GitHub/DigitalScholarlyEditing_Renzo-Lucia/assets/xml/renzolucia.xsl'
 
 # Carica i file
 xml = etree.parse(xml_file)
@@ -15,7 +15,7 @@ transform = etree.XSLT(xsl)
 result = transform(xml)
 
 # Salva l’output in un file HTML
-output_file = 'improvedoutput.html'
+output_file = 'output.html'
 with open(output_file, 'wb') as f:
     f.write(etree.tostring(result, pretty_print=True, method='html', encoding='UTF-8'))
 
